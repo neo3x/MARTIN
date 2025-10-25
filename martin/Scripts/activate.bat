29d2ceb18b20b7d146662549ccc93fbd21e55bdc
Francisco <panxox1983@gmail.com>
2025-10-25 14:00:31 -0300

    commit env

--- martin/Scripts/activate.bat
+++ martin/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\IA\MARTIN\martin
+set VIRTUAL_ENV=C:\Users\forti\Documents\GitHub\MARTIN\martin
 
 if not defined PROMPT set PROMPT=$P$G
 
